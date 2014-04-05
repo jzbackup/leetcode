2728b89b6e81cce6a5719409da171e0c51914978
freikorps <zhaojinhust@gmail.com>
2014-04-05 01:53:55 -0500

    my first commit

---
+++ pow.h
@@ -0,0 +1,22 @@
+/*This is my first file with Github
+Problem: Pow(x, n)
+Notes: Implement Pow(x, n),
+
+*/
+
+
+class Solution{
+public:
+    double pow(double x, int n) {
+    	//for INT_MIN, -n is invalid, so handle this case first
+        if(n == INT_MIN) return 1/(x*pow(x, INT_MAX));
+    	if(n < 0) return 1/pow(x, -n);
+        if(n == 0)return 1;
+        if(n == 1)return x;
+
+        double pre = pow(x, n/2);
+        if(n%2 == 0)
+        	return pre*pre;
+        else
+        	return pre*pre*x;
+    }};
